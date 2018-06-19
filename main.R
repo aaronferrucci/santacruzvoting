a7c0bc5b26873f6c742b661cff6514e159530740
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-06-18 22:22:47 -0700

    Thinner white lines, extract more data into the data.frame.

--- main.R
+++ main.R
@@ -12,11 +12,17 @@ dataDF$Precinct <- as.integer(as.character(dataDF$Precinct))
 source("parse_voter_data.R")
 # 'merge' reorders the data.frame, which ruins the choropleth...
 # dataDF2 <- merge(sort=F, dataDF, turnout_by_precinct[, c("Precinct", "Turnout")], by="Precinct")
-dataDF$Turnout <- turnout_by_precinct[match(dataDF$Precinct, turnout_by_precinct$Precinct, nomatch=which(turnout_by_precinct$Turnout == min(turnout_by_precinct$Turnout))[1]), "Turnout"]
+dataDF$Turnout <-
+  turnout_by_precinct[match(dataDF$Precinct, turnout_by_precinct$Precinct, nomatch=which(turnout_by_precinct$Turnout == min(turnout_by_precinct$Turnout))[1]), "Turnout"]
+dataDF$'Registered Voters' <- turnout_by_precinct$'Registered Voters'[match(dataDF$Precinct, turnout_by_precinct$Precinct)]
+dataDF$'Registered Voters' <- ifelse(is.na(dataDF$'Registered Voters'), 0, dataDF$'Registered Voters')
 
-ggData <- ggplot(data=dataDF, aes(x=long, y=lat, group=group, fill=Turnout)) +
+dataDF$'Ballots Cast' <- turnout_by_precinct$'Ballots Cast'[match(dataDF$Precinct, turnout_by_precinct$Precinct)]
+dataDF$'Ballots Cast' <- ifelse(is.na(dataDF$'Ballots Cast'), 0, dataDF$'Ballots Cast')
+
+ggData <- ggplot(data=dataDF, aes(x=long, y=lat, group=group, fill=`Turnout`)) +
   geom_polygon() +
-  geom_path(color="white") +
+  geom_path(color="white", size=0.1) +
   scale_fill_gradient(low="darkgreen", high="green") +
   coord_equal() +
   theme(axis.text=element_blank())
